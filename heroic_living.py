266d64d329702e85273a33961ebc81a90575f7ba
Adrian Cunanan <adrian@adriancunanan.com>
2022-11-19 11:04:43 -0500

    Initial commit

---
+++ heroic_living.py
@@ -0,0 +1,16 @@
+# The #1 algorithm? One word: Areté.
+'''
+ALGORITHM #1: Areté
+IF ( ALIVE ) {
+    STRIVE_TO( EXPRESS_BEST_VERSION( SELF ) );
+}
+'''
+def live_with_areté():
+    while self == alive:
+        express_best_version(self)
+
+def main():
+    live_with_areté()
+
+if __name__ == "__main__":
+    main()
---
+++ the_construct.py
@@ -0,0 +1,58 @@
+'''
+"How Am I THAT?"
+https://www.heroic.us/optimize/plus-one/how-am-i-that
+'''
+IF ( TRIGGERED ) {
+    HOW_AM_I_THAT();
+}
+
+'''
+GOAL: Practice managing reaction (Fear/Anxiety, Anger/Frustration, Digust)
+and future projection.
+- Avoid heart medication. This is within control. #selfcare
+- "Most difficult person you're dealin with is you"
+
+"Frankl on Freedom"
+https://www.optimize.me/plus-one/frankl-on-freedom
+'''
+IF ( REACTION_TO_STIMULUS != COMFORTABLE) {
+    BREATHE_AND_STOP(); '''ATCQ + Stop Thinking + Flip the Switch'''
+    LISTEN(); '''Like a Therapist'''
+    UNDERSTAND(); '''Seek to Understand Before Being Understood'''
+    RESPOND();
+}
+
+IF ( ANXIETY > 0) {
+    '''No breath, no life!'''
+    BREATHE_LESS();
+    BREATHE_RIGHT();
+
+    '''
+    PM RITUAL BOOKEND
+    Best Day Starts the Night Before.
+    '''
+    PRIORITIZE_SLEEP();
+    SHUTDOWN_COMPLETE();
+    DIGITAL_SUNSET();
+    
+    '''
+    AM RITUAL BOOKEND
+    Start the day with focusing on what is within our control.
+    Be creative before reactive. #MakeYourself #MasterpieceDays
+    '''
+    HYDRATE();
+    AM_MEDITATION();
+    AM_MOVEMENT();
+    HEALTHY_BREAKFAST();
+    
+    '''
+    INPUT MODULATION
+    None are as important as you
+    '''
+    REDUCE_INPUTS();
+}
+
+'''
+What is the #1 thing you can start doing?
+What is the #1 thing you can stop doing?
+'''
