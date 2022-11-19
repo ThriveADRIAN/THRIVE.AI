6a18fe7a60b9d507e45cf45ef32c97a1ae6fff85
Adrian Cunanan <adrian@adriancunanan.com>
2022-11-19 11:32:38 -0500

    Add Content Links

--- the_construct.py
+++ the_construct.py
@@ -10,26 +10,35 @@ IF ( TRIGGERED ) {
 GOAL: Practice managing reaction (Fear/Anxiety, Anger/Frustration, Digust)
 and future projection.
 - Avoid heart medication. This is within control. #selfcare
-- "Most difficult person you're dealin with is you"
+- "Most difficult person you are dealing with is you"
 
-"Frankl on Freedom"
-https://www.optimize.me/plus-one/frankl-on-freedom
+The Fastest Way to Optimize
+https://www.optimize.me/plus-one/the-fastest-way-to-optimize
+- What is the #1 thing you can start doing?
+- What is the #1 thing you can stop doing?
+
+Just Execute Your Game Plan
+https://www.optimize.me/plus-one/just-execute-your-game-plan
 '''
 IF ( REACTION_TO_STIMULUS != COMFORTABLE) {
     BREATHE_AND_STOP(); '''ATCQ + Stop Thinking + Flip the Switch'''
-    LISTEN(); '''Like a Therapist'''
-    UNDERSTAND(); '''Seek to Understand Before Being Understood'''
-    RESPOND();
+    LISTEN(); '''How to listen like a therapist: 4 secret skills https://www.youtube.com/watch?v=UVN96JhDOmg'''
+    UNDERSTAND(); '''(Habit 5) Think Win/Win +https://www.optimize.me/plus-one/think-win-win''''
+    RESPOND(); '''"Frankl on Freedom" https://www.optimize.me/plus-one/frankl-on-freedom'''
 }
 
 IF ( ANXIETY > 0) {
-    '''No breath, no life!'''
+    '''
+    No breath, no life!
+    Optimal Breathing 101 https://www.optimize.me/101/optimal-breathing
+    '''
     BREATHE_LESS();
     BREATHE_RIGHT();
 
     '''
     PM RITUAL BOOKEND
     Best Day Starts the Night Before.
+    PM Counts Twice https://www.optimize.me/plus-one/pm-counts-twice
     '''
     PRIORITIZE_SLEEP();
     SHUTDOWN_COMPLETE();
@@ -39,20 +48,17 @@ IF ( ANXIETY > 0) {
     AM RITUAL BOOKEND
     Start the day with focusing on what is within our control.
     Be creative before reactive. #MakeYourself #MasterpieceDays
+    AM + PM Bookends https://www.optimize.me/plus-one/am-pm-bookends
     '''
+    WAKE_WITH_PURPOSE(); '''Ikigai https://www.optimize.me/plus-one/ikigai'''
     HYDRATE();
-    AM_MEDITATION();
-    AM_MOVEMENT();
-    HEALTHY_BREAKFAST();
-    
+    AM_MEDITATION(); '''A 2:43 Heroic Meditation https://www.optimize.me/plus-one/a-243-heroic-meditation'''
+    AM_MOVEMENT(); '''Movement > Exercise https://www.optimize.me/plus-one/movement-exercise'''
+    HEALTHY_BREAKFAST(); '''Eat Food https://www.optimize.me/plus-one/food-rule-1-eat-food'''
+    OSCILLATE(); '''Start Gymboss Plus Timer 1% (14:24) https://www.amazon.com/dp/B07QK9WBW6/ref=twister_B08B1RMCZ7?_encoding=UTF8&psc=1'''
     '''
     INPUT MODULATION
     None are as important as you
     '''
     REDUCE_INPUTS();
 }
-
-'''
-What is the #1 thing you can start doing?
-What is the #1 thing you can stop doing?
-'''
